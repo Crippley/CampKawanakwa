289459c2736058045026c52d513b85dde06112b4
Mihael Diklic <sonycsghost@gmail.com>
2021-09-01 05:29:39 +0200

    Added AgentVsAgent and HumanVsAgent scenes

--- Assets/Scripts/Classes/Player.cs
+++ Assets/Scripts/Classes/Player.cs
@@ -89,7 +89,7 @@ namespace Entities
                 actionMask.SetActionEnabled(3, 1, true);
         }
 
-        /*public override void Heuristic(in ActionBuffers actionsOut)
+        public override void Heuristic(in ActionBuffers actionsOut)
         {
             ActionSegment<int> discreteActions = actionsOut.DiscreteActions;
 
@@ -122,7 +122,7 @@ namespace Entities
                 discreteActions[3] = 1;
             else
                 discreteActions[3] = 0;
-        }*/
+        }
 
         public override void OnActionReceived(ActionBuffers actions)
         {
